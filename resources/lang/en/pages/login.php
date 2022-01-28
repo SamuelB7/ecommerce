3df7c012bd4b6df1cee089216ea85d8b5589cf04
Samuel Belo <belo.samuel@gmail.com>
2022-01-28 07:45:13 -0300

    initial commit

---
+++ resources/lang/en/pages/login.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'email' => 'Email Address',
+    'password' => 'Password',
+    'remember_me' => 'Remember Me',
+    'forgot_password' => 'Forgot Your Password?'
+];
---
+++ resources/lang/pt-br/pages/login.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'email' => 'Endereço de Email',
+    'password' => 'Senha',
+    'remember_me' => 'Lembrar meu acesso',
+    'forgot_password' => 'Esqueceu a sua senha?'
+];
