8684e9f929e0a19ec491cb62fd278716d3c553bb
Samuel Belo <belo.samuel@gmail.com>
2022-02-08 08:30:59 -0300

    Localization for register page, english and portuguese-br

---
+++ resources/lang/en/pages/register.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'name' => 'Name',
+    'register' => 'Register',
+    'email' => 'Email Address',
+    'password' => 'Password',
+    'confirm_password' => 'Confirm Password',
+    'register_btn' => 'Register'
+];
---
+++ resources/lang/pt-br/pages/register.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'name' => 'Nome completo',
+    'register' => 'Cadastre-se',
+    'email' => 'Endereço de email',
+    'password' => 'Senha',
+    'confirm_password' => 'Confrimar senha',
+    'register_btn' => 'Cadastre-se'
+];
