707381e4c4aa6edb0669ec324872467537949d74
Samuel Belo <belo.samuel@gmail.com>
2022-04-14 21:22:18 -0300

    database seeder added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,12 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +18,76 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            [
+                'name' => 'Admin',
+                'email' => 'admin@email.com',
+                'password' => Hash::make('123456789'),
+                'role' => 'admin',
+                'created_at' => Carbon::now()
+            ],
+            [
+                'name' => 'Fulano de Tal',
+                'email' => 'maria@email.com',
+                'password' => Hash::make('123456789'),
+                'role' => 'agent',
+                'created_at' => Carbon::now()
+            ],
+            [
+                'name' => 'Siclano de Tal',
+                'email' => 'siclano@email.com',
+                'password' => Hash::make('123456789'),
+                'role' => 'user',
+                'created_at' => Carbon::now()
+            ],
+        ]);
+
+        DB::table('categories')->insert([
+            [
+                'name' => 'Computadores e Notebooks',
+                'created_at' => Carbon::now()
+            ],
+            [
+                'name' => 'Smartphones',
+                'created_at' => Carbon::now()
+            ],
+            [
+                'name' => 'Tablets',
+                'created_at' => Carbon::now()
+            ],
+            [
+                'name' => 'Hardware',
+                'created_at' => Carbon::now()
+            ],
+        ]);
+
+        $faker = Faker\Factory::create();
+
+        for($i = 1; $i < 501; $i++) {
+            DB::table('products')->insert([
+                [
+                    'name' => $faker->text(15),
+                    'description' => $faker->text(),
+                    'category_id' => rand(1,4),
+                    'price' => rand(100, 5000),
+                    'quantity' => rand(0, 10000),
+                    'created_at' => Carbon::now()
+                ]
+            ]);
+
+            DB::table('products_images')->insert([
+                [
+                    'image' => 'https://place-hold.it/500',
+                    'product_id' => $i,
+                    'created_at' => Carbon::now()
+                ],
+                [
+                    'image' => 'https://place-hold.it/500',
+                    'product_id' => $i,
+                    'created_at' => Carbon::now()
+                ],
+            ]);
+        }
+
     }
 }
--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -55,6 +55,7 @@
                     @endforeach
                     </tbody>
                 </table>
+                {{ $products->links('pagination::bootstrap-4') }}
             </div>
         </div>
     </section>
