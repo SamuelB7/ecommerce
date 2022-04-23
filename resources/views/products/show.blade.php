13e69075e8dfedd8d106bfdd4fe4dfc8c92fcc0e
Samuel Belo <belo.samuel@gmail.com>
2022-04-23 04:36:32 -0300

    progress on product show

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -2,6 +2,27 @@
 
 @section('content')
 <div class="container">
-    <h1>{{ $product->name }}</h1>
+    <div class="row">
+        <div class="col">
+            <img width="500px" height="500px" src="{{ url($product->images[0]->image) }}" alt="" srcset="">
+        </div>
+        <div class="col">
+            <h3>{{ $product->name }}</h3>
+            <h5>R$ {{ $product->price }}</h5>
+            <p>{{ $product->description }}</p>
+            <form class="d-flex align-items-center gap-2" action="">
+                <label class="m-0" for="quantity_bought">Quantidade</label>
+                <input style="width: 4rem;" type="number" class="form-control" name="quantity_bought" id="quantity_bought" value="1">
+                <button class="btn btn-success">COMPRAR</button>
+            </form>
+        </div>
+    </div>
+    <div class="row row-cols-auto mt-2">
+        @foreach($product->images as $image)
+            <div class="col">
+                <img width="200px" height="200px" class="img-thumbnail" src="{{ url($image->image) }}">
+            </div>
+        @endforeach
+    </div>
 </div>
 @endsection
